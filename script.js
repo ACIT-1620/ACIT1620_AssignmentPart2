3fa86067ef69e8b0e6ec3c5bc0cdca9b527e2bcc
Lucas Angelozzi <lucasangelozzi32@gmail.com>
2021-11-26 18:03:53 -0800

    First 3 functions for js finished. Index page stuff done

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-/* This file is where all your work should go for Assignment 2.  Please remove this comment. */
+const contactsDiv = document.querySelector('.main')
+
+let contactList = [
+    {
+      name: "Roberta Dobbs",
+      phone: "778-555-1234",
+      address: "101 Main St, Anytown, USA",
+      email: "subgenius@slack.example.com",
+    }, 
+    {
+      name: "Bugs Bunny",
+      phone: "123-867-5309",
+      address: "Warner Brothers Animation Lot",
+      email: "whatsup@doc.example.com",
+    },
+  ]
+
+function cleanUpIndex() {
+
+    while (contactsDiv.firstChild) {
+        contactsDiv.firstChild.remove()
+    }
+}
+
+function createSingleIndex(obj) {
+    var link = document.createElement('a')
+    var indexCardDiv = document.createElement('div')
+    indexCardDiv.classList.add('contact')
+    var paragraph = document.createElement('p')
+    
+    paragraph.innerHTML = obj.name
+    indexCardDiv.appendChild(paragraph)
+    link.appendChild(indexCardDiv)
+    link.href = 'page3.html'
+
+    return link
+}
+
+function renderIndex(array) {
+    for (let i = 0; i < array.length; i++) {
+        contactsDiv.appendChild(createSingleIndex(contactList[i]))
+    }
+}
+
